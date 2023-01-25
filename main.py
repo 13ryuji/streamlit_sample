6cc9f22a458bba4c98c9262b7775a3f7ace2a145
13ryuji <13ryuji@gmail.com>
2023-01-26 08:49:28 +0900

    add url

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Ryuji streamlit sample ---')
+st.title('松岡　竜嗣　ポートフォリオ')
+st.write('HTML/CSSの勉強')
 st.write('https://ryuji-web-sample.web.app/')
+
+st.title('ネタ')
+st.write('Webスクレイピング')
+st.write('さわやかビッグデータプロジェクト')
