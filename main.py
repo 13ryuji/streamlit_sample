82cc52b4c5c62f97a1582084f30d8ded4d6a8966
13ryuji <13ryuji@gmail.com>
2023-02-27 08:25:04 +0900

    add app

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import pandas as pd
 import numpy as np
 
 biz_list = [
-    'web制作',
-    'SNS運用',
-    'アンケート作成&集計'
+    'アンケート作成&集計',
     'RPA',
-    '動画制作'
+    '動画制作',
+    'SNS運用',
+    'web制作',
 ]
 
 achievement_list = [
+    'https://13ryuji-mtg-limited-balance-app-yn7767.streamlit.app/',
     'https://ryuji-web-sample.web.app/',
     'https://lookerstudio.google.com/u/0/reporting/f64eb01b-92d8-42a0-b4bf-116d3bd2395e/page/7OeBD',
     'https://docs.google.com/forms/d/e/1FAIpQLSdLjVrNiLXZ_GOnqe-BpfOaRzOdUBy0F3ztM9tGsBrLDT4qAA/viewform',
@@ -30,8 +31,13 @@ bonz_list = [
 seed_list = [
     'さわやかビッグデータプロジェクト',
     '地域メタゲーム集計ツール',
+    'アリーナコード一発コピペ',
     'ローカル線めぐりパーフェクトガイド',
     '商工会のプロと学ぶ独立起業＆副業',
+]
+
+sns_list = [
+    'https://qiita.com/13ryuji'
 ]
 st.title('Maverick')
 st.header('事業内容')
@@ -43,10 +49,14 @@ st.header('実績')
 for i in achievement_list:
     st.markdown('- ' + i)
 
-st.subheader('ボンズ　デモサイト')
-for i in bonz_list:
+st.header('SNS')
+for i in sns_list:
     st.markdown('- ' + i)
 
 st.header('ネタ')
 for i in seed_list:
+    st.markdown('- ' + i)
+
+st.subheader('ボンズ　デモサイト')
+for i in bonz_list:
     st.markdown('- ' + i)
